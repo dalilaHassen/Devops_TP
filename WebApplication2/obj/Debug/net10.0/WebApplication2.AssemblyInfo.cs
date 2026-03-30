5d9836dbb78077504338826a9231528880073435
Gueddes Imen <imengds@gmail.com>
2026-03-30 11:33:57 +0100

    modification q3 part 6

--- WebApplication2/obj/Debug/net10.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net10.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c450e82d9e61564fb4049094694cc16964f92dbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325be4e8eaa46ffb3465470cc5ef21ad76534848")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
